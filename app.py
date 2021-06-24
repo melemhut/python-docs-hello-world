cc9fb3d9c9ae4588e65d8d5b2d69c504736ecfbc
melemhut <86409891+melemhut@users.noreply.github.com>
2021-06-24 15:33:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import dash
-#import dash_core_components as dcc
-#import dash_html_components as html
+import dash_core_components as dcc
+import dash_html_components as html
 
 app = Flask(__name__)
 
