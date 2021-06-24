e969bb1b39687b0a540fb34370408237fc530bd4
melemhut <86409891+melemhut@users.noreply.github.com>
2021-06-24 17:38:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ from io import StringIO
 
 app = Flask(__name__)
 
+def connexion_blob_storage():
+    return(BlockBlobService(account_name='prodthermalmanagementst', account_key='Az2kaMF26Vwd3lzztIIiteX2Ytbmn8tIriazHTu74Lm1O8lyui9i8o8qNdaZ1ceZ7Kh+Nn7BaxnOtLIBQBbK9g=='))
+    
+block_blob_service = connexion_blob_storage()
+blobstring = block_blob_service.get_blob_to_text("processeddata","Cu_Cu_p.csv").content
+df = pd.read_csv(StringIO(blobstring), sep=";", decimal=".",header=0)
+
 @server.route("/")
 def hello():
     return "Hello from azure"
