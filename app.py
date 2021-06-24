b670d21884210b3d37496a94c62e0020d119fb8d
melemhut <86409891+melemhut@users.noreply.github.com>
2021-06-24 14:43:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import seaborn as sns
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
 app = Flask(__name__)
 
 @app.route("/")
